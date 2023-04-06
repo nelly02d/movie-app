6e559a9bbaa4cc82a6db8a12e790ab2019e39488
Arnel <nelly02d@gmail.com>
2023-04-06 16:55:39 -0400

    add feature component to put the star rating on it

---
+++ src/app/feature/star-rating/star-rating.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-star-rating',
+  templateUrl: './star-rating.component.html',
+  styleUrls: ['./star-rating.component.scss']
+})
+export class StarRatingComponent {
+  @Input() rating: any;
+  @Input() isReadOnly: boolean = false;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
+
+  ariaValueText(current: number, max: number) {
+		return `${current} out of ${max} hearts`;
+	}
+}
