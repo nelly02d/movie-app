17a4d28234f59c88facf7edeca3f81bacd6c4274
Arnel <nelly02d@gmail.com>
2023-04-06 16:58:39 -0400

    add pupular movies and theatre movies on home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NgbRatingModule, NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Router, RouterModule } from '@angular/router';
 
 
 
@@ -13,26 +14,39 @@ import { NgbRatingModule, NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 export class HomeComponent {
 
   trendingMovies: any; 
+  theatreMovies: any;
+  popularMovies: any;
 
-  constructor(private http: HttpClient, config: NgbRatingConfig) {
-    config.max = 5;
-		config.readonly = true;
+  constructor(private http: HttpClient, private router: Router) {
   }
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
     this.getTrendingMovies();
+    this.getTheatreMovies();
+    this.getPopularMovies();
   }
 
   getTrendingMovies() {
     this.http.get('http://localhost:4200/assets/data/trending-movies.json').subscribe((movies) => { //need to add the http client on app.module
       this.trendingMovies = movies
-      console.log(this.trendingMovies)
     })
   }
-  
-  ariaValueText(current: number, max: number) {
-		return `${current} out of ${max} hearts`;
-	}
+
+  getTheatreMovies() {
+    this.http.get('http://localhost:4200/assets/data/theatre-movies.json').subscribe((movies) => { //need to add the http client on app.module
+      this.theatreMovies = movies
+    })
+  }
+
+  getPopularMovies() {
+    this.http.get('http://localhost:4200/assets/data/popular-movies.json').subscribe((movies) => { //need to add the http client on app.module
+      this.popularMovies = movies
+    })
+  }
+
+  goToMovie(type: string, id: string) {
+    this.router.navigate(['movie', type, id])
+  }
 }
